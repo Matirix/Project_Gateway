bc1d92c5e99d2571ee4557c698391e2ae1075279
April Cheng <acheng118@my.bcit.ca>
2023-06-09 21:06:22 -0700

    Added test Patient

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:prj_gateway/patient.dart';
 import 'package:prj_gateway/sign_in.dart';
+import 'open_ai.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+
+  // ------------------ Create a Patient instance for testing ------------------//
+  Patient patient = Patient(
+    name: 'John Doe',
+    age: 30,
+    dob: DateTime(1992, 10, 15),
+    sex: 'M',
+    insuranceProvider: 'ABC Insurance',
+    medications: [
+      Drug(name: 'Fluoxetine', dosage: "2.5mg"),
+      Drug(name: 'Loratadine', dosage: "0.5mg"),
+    ],
+    doctorNotes: [
+      DoctorNote(
+        date: DateTime(2023, 6, 10),
+        noteId: 'N001',
+        doctorName: 'Dr. Smith',
+        content: 'The purpose of this note is to provide information regarding their respiratory infection and necessary medical attention.',
+      ),
+    ],
+    medicalHistory: ['Diagnosed in 2012, controlled through diet and exercise. No medication required.',
+      'Known allergy to penicillin.'],
+  );
+
+  // ------------------ Test calling callOpenAI in open_ai.dart file ------------------//
+  try {
+    final response = await callOpenAI(1, patient);
+    debugPrint('API response: $response');
+  } catch (error) {
+    debugPrint('Error: $error');
+  }
 }
 
 class MyApp extends StatelessWidget {
