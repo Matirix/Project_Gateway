518d2dc198eba44f3015d3809e912613f7eda6f5
Matthew <Matirix@live.com>
2023-06-09 17:15:56 -0700

    chagned the home_page to refelct the figma design

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../utils/custom_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +11,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: CustomAppBar(),
-      body: Center(
-        child: Text('Home Page'),
+        // appBar: CustomAppBar(),
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 100,
+          ),
+          Icon(
+            Icons.family_restroom_outlined,
+            color: Colors.blue,
+            fill: 0.0,
+          ),
+          Text(
+            "Paulius Russel, MD",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          Text(
+            "Family Doctor",
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
