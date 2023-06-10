d8ddd7548ea15680c5c1639cb51419d336807a03
Matthew <Matirix@live.com>
2023-06-09 18:15:25 -0700

    added the colors and modifed the home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prj_gateway/utils/app_colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,34 +11,47 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         // appBar: CustomAppBar(),
-        body: Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-          Icon(
-            Icons.family_restroom_outlined,
-            color: Colors.blue,
-            fill: 0.0,
-          ),
-          Text(
-            "Paulius Russel, MD",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
+        body: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFFFFFFFF), // Ending color (#FFFFFF)
+            Color(0xFFEEF2FF), // Starting color (#EEF2FF)
+          ],
+          stops: [0.0, 1.0],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 100,
             ),
-          ),
-          Text(
-            "Family Doctor",
-            style: TextStyle(
-              fontSize: 16,
+            Icon(
+              Icons.family_restroom_outlined,
+              color: AppColors.secondary,
+              fill: 0.0,
             ),
-          )
-        ],
+            const Text(
+              "Paulius Russel, MD",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const Text(
+              "Family Doctor",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
