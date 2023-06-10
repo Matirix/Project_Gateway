a4a72442b4f413a225aeb8ca809b19f71b20923a
Gareth Ng <ngys.gareth@gmail.com>
2023-06-10 04:58:21 -0700

    added animation for logo and text

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,5 +1,6 @@
 // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../utils/navigation_page.dart';
 // import '../api/auth.dart';
 // import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
@@ -11,201 +12,129 @@ class SignIn extends StatefulWidget {
   State<SignIn> createState() => _SignInState();
 }
 
-class _SignInState extends State<SignIn> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
 
-  //TODO: display this error messsage somewhere
-  //String? _errorMessage = '';
+  late AnimationController _controller;
+  late Animation<Color?> _bottomColorAnimation;
+  late Animation<Color?> _topColorAnimation;
+  late Animation<Color?> _textColorAnimation;
+  late Animation<double> _logoAnimation;
 
-  // Future<void> signInWithEmailAndPassword() async {
-  //   try {
-  //     // await Auth().signInWithEmailAndPassword(
-  //     //     email: _emailController.text, password: _passwordController.text);
-  //     //TODO:Remove afterr test
-  //     await Auth().signInWithEmailAndPassword(
-  //         email: "ryanstolys+test@gmail.com", password: "1ioPerform");
-  //   } on FirebaseAuthException {
-  //     setState(() {
-  //       //_errorMessage = e.message;
-  //     });
-  //   }
-  // }
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 4));
+
+    _bottomColorAnimation = TweenSequence(
+      [
+        TweenSequenceItem(
+          tween: ColorTween(begin: Color(0xFFFFFFFF), end: Color(0xFF549DC6)),
+          weight: 1,
+        ),
+        TweenSequenceItem(
+          tween: ColorTween(begin: Color(0xFF549DC6), end: Color(0xFFFFFFFF)),
+          weight: 1,
+        ),
+      ],
+    ).animate(_controller);
+
+    _topColorAnimation = TweenSequence(
+      [
+        TweenSequenceItem(
+          tween: ColorTween(begin: Color(0xFF549DC6), end: Color(0xFFFFFFFF)),
+          weight: 1,
+        ),
+        TweenSequenceItem(
+          tween: ColorTween(begin: Color(0xFFFFFFFF), end: Color(0xFF549DC6)),
+          weight: 1,
+        ),
+      ],
+    ).animate(_controller);
+
+    _textColorAnimation = ColorTween(begin: Colors.white, end: Colors.black).animate(_controller);
+
+    _logoAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    // _logoAnimation = Tween<String>(begin: 'assets/logo_white.svg', end: 'assets/logo_black.svg')
+    //     .animate(_controller);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the animation controller when the widget is removed from the tree
+    super.dispose();
+  }
+
+  String getLogoPath() {
+    if (_logoAnimation.value >= 0.5) {
+      return 'assets/logo_black.svg';
+    } else {
+      return 'assets/logo_white.svg';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 100,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'LOG IN',
-                        style: TextStyle(
-                          color: Color(0xFF155B94),
-                          fontSize: 35,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        'Welcome back Doctor',
-                        style: TextStyle(
-                          color: const Color(0xFF155B94).withOpacity(0.8),
-                          fontSize: 18,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              const Image(
-                image: AssetImage('assets/logo.png'),
-                height: 100,
-                width: 400,
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              const Text('Username:',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFF155B94),
-                  )),
-              const SizedBox(
-                height: 5,
-              ),
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  filled: true,
-                  fillColor: const Color(0xFFF2F2F2),
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                  contentPadding: const EdgeInsets.only(
-                    top: -15,
-                    left: 10,
-                    right: 10,
-                  ),
-                  labelText: 'Username',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius:
-                        BorderRadius.circular(10), // Set border radius
-                    borderSide: const BorderSide(
-                      color: Color(0xfff1f1f1), // Customize border color
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius:
-                        BorderRadius.circular(10), // Set border radius
-                    borderSide: const BorderSide(
-                      color:
-                          Color(0xfff1f1f1), // Customize focused border color
-                    ),
-                  ),
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context,_) {
+            return Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [_bottomColorAnimation.value!, _topColorAnimation.value!],
+                    )
                 ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              const Text('Password:',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFF155B94),
-                  )),
-              const SizedBox(
-                height: 5,
-              ),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                  filled: true,
-                  fillColor: const Color(0xFFF2F2F2),
-                  contentPadding: const EdgeInsets.only(
-                    top: -15,
-                    left: 10,
-                    right: 10,
-                  ),
-                  labelText: 'Password',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius:
-                        BorderRadius.circular(10), // Set border radius
-                    borderSide: const BorderSide(
-                      color: Color(0xfff1f1f1), // Customize border color
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 250,
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius:
-                        BorderRadius.circular(10), // Set border radius
-                    borderSide: const BorderSide(
-                      color:
-                          Color(0xfff1f1f1), // Customize focused border color
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 55.0,
-              ),
-              Column(
-                children: [
-                  Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(300, 45),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        backgroundColor: const Color(0xFF155B94),
-                      ),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => NavigationPage()));
+                    AnimatedBuilder(
+                      animation: _controller,
+                      builder: (context, _) {
+                        return SvgPicture.asset(
+                          getLogoPath(),
+                          width: 20,
+                          height: 20,
+                          color: _textColorAnimation.value,
+                        );
                       },
-                      child: const Text(
-                        'LOG IN',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
-                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    AnimatedBuilder(
+                      animation: _controller,
+                      builder: (context, _) {
+                        return Text(
+                          'GATEWAY',
+                          style: TextStyle(
+                            fontFamily: 'Barlow',
+                            fontSize: 50,
+                            color: _textColorAnimation.value,
+                            letterSpacing: 4.0,
+                          ),
+                        );
+                      }
+                    )
+                  ],
+                )
+            );
+          }
+
+
+        )
+
       ),
     );
   }
